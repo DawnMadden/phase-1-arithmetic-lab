0cfc5b8afa8d311ff7219b3a88e18f2c951bb653
Dawn Madden <coachdawnmadden@gmail.com>
2023-09-04 15:21:56 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+let num1 =10;
+let num2 =6.2;
+let multiply = num1 * num2;
+var random = Math.ceil(Math.random()*10) +1;
+let num3=14;
+let num4=5;
+var mod = num3 % num4;
+var max=Math.max(1,10,20);
